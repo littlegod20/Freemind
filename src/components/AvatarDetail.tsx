f3fe93da345708076038f8285471b80e93cdc425
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2024-12-14 14:38:55 +0000

    modified AvatarDetails

--- src/components/AvatarDetail.tsx
+++ src/components/AvatarDetail.tsx
@@ -6,7 +6,7 @@ type AvatarProps = Pick<TableDetailsTypes, "email" | "name">;
 const AvatarDetail = ({ name, email }: AvatarProps) => {
   return (
     <>
-      <div className="w-full flex justify-between">
+      <div className="w-full flex flex-col sm:flex-row justify-between">
         <div className="flex items-center gap-2 p-2">
           <p>Avatar</p>
           <div>
@@ -14,10 +14,12 @@ const AvatarDetail = ({ name, email }: AvatarProps) => {
             <p className="text-slate-500 text-xs font-light"> {email} </p>
           </div>
         </div>
-        <Button
-          title="Edit user details"
-          className="bg-green-active text-white border-0 shadow-inner hover:bg-green-700"
-        />
+        <div className="flex w-full justify-end pt-3 sm:pt-0">
+          <Button
+            title="Edit user details"
+            className="bg-green-active text-white border-0 shadow-inner hover:bg-green-700"
+          />
+        </div>
       </div>
     </>
   );
