3a26582c695da821c8c984d62e706c78f6511279
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2024-12-14 15:34:28 +0000

    installed shadcn and finished working on the user details ui

--- src/components/AvatarDetail.tsx
+++ src/components/AvatarDetail.tsx
@@ -1,14 +1,22 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { TableDetailsTypes } from "../utils/types";
 import Button from "./Button";
 
-type AvatarProps = Pick<TableDetailsTypes, "email" | "name">;
+interface AvatarProps extends TableDetailsTypes {
+  image?: string;
+}
 
-const AvatarDetail = ({ name, email }: AvatarProps) => {
+const AvatarDetail = ({ name, email, image }: AvatarProps) => {
   return (
     <>
       <div className="w-full flex flex-col sm:flex-row justify-between">
-        <div className="flex items-center gap-2 p-2">
-          <p>Avatar</p>
+        <div className="flex items-center gap-3 p-2">
+          <Avatar>
+            <AvatarImage src={image} />
+            <AvatarFallback className="bg-green-300 px-4 py-3 rounded-full bg-opacity-50 font-bold text-green-900 text-xl">
+              {name?.split("")[0]}
+            </AvatarFallback>
+          </Avatar>
           <div>
             <p className="font-bold">{name}</p>
             <p className="text-slate-500 text-xs font-light"> {email} </p>
