5dd8e473c5f74ec08ab9025a00361d139f9bacec
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-01-08 15:40:23 +0000

    modified project to include lazy loading of pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import DashboardLayout from "./layouts/DashboardLayout";
-import Clients from "./pages/ClientPages/Clients";
-import Invitations from "./pages/Invitations";
-import Analytics from "./pages/Analytics/Analytics";
-import Prefixed_Prompts from "./pages/Prefixed/Prefixed_Prompts";
-import Invite_Mail_Settings from "./pages/settings/Invite_Mail_Settings";
-import Profile_Settings from "./pages/settings/Profile_Settings";
-import Users from "./pages/UserPages/Users";
-import UserDetails from "./pages/UserPages/UserDetails";
-import ClientDetails from "./pages/ClientPages/ClientDetailsPage";
+// import Clients from "./pages/ClientPages/Clients";
+// import Invitations from "./pages/Invitations";
+// import Analytics from "./pages/Analytics/Analytics";
+// import Prefixed_Prompts from "./pages/Prefixed/Prefixed_Prompts";
+// import Invite_Mail_Settings from "./pages/settings/Invite_Mail_Settings";
+// import Profile_Settings from "./pages/settings/Profile_Settings";
+// import Users from "./pages/UserPages/Users";
+// import UserDetails from "./pages/UserPages/UserDetails";
+// import ClientDetails from "./pages/ClientPages/ClientDetailsPage";
+import { lazy } from "react";
+
+const Clients = lazy(() => import("./pages/ClientPages/Clients"));
+const Invitations = lazy(() => import("./pages/Invitations"));
+const Analytics = lazy(() => import("./pages/Analytics/Analytics"));
+const Prefixed_Prompts = lazy(
+  () => import("./pages/Prefixed/Prefixed_Prompts")
+);
+const Invite_Mail_Settings = lazy(
+  () => import("./pages/settings/Invite_Mail_Settings")
+);
+const Profile_Settings = lazy(
+  () => import("./pages/settings/Profile_Settings")
+);
+const Users = lazy(() => import("./pages/UserPages/Users"));
+const UserDetails = lazy(() => import("./pages/UserPages/UserDetails"));
+const ClientDetails = lazy(
+  () => import("./pages/ClientPages/ClientDetailsPage")
+);
 
 function App() {
   return (
@@ -18,7 +37,10 @@ function App() {
           <Route index element={<Users />} />
           <Route path="/user-details/:id" element={<UserDetails />} />
           <Route path="/clients" element={<Clients />} />
-          <Route path="/clients/client-details/:id" element={<ClientDetails />} />
+          <Route
+            path="/clients/client-details/:id"
+            element={<ClientDetails />}
+          />
           <Route path="/invitations" element={<Invitations />} />
           <Route path="/analytics" element={<Analytics />} />
           <Route path="/prefixed prompts" element={<Prefixed_Prompts />} />
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,15 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Dashboard from "../components/widgets/Dashboard";
 import MobileDashboard from "@/components/widgets/MobileDashboard";
+import { Suspense } from "react";
 
 const DashboardLayout = () => {
   return (
     <main className="flex md:flex-row flex-col w-screen h-screen">
       <Dashboard />
-      <MobileDashboard/>
-      <section className="md:py-6 py-4 px-4 flex-1 md:w-3/5 h-screen overflow-scroll">
-        <Outlet />
-      </section>
+      <MobileDashboard />
+      <Suspense fallback={<p>Loading...</p>}>
+        <section className="md:py-6 py-4 px-4 flex-1 md:w-3/5 h-screen overflow-scroll">
+          <Outlet />
+        </section>
+      </Suspense>
     </main>
   );
 };
