541bb8df55401affa509f1c9b4e6e7b899165526
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-01-08 14:00:43 +0000

    modified code to have the state of clients in slice

--- src/store/slices/clientSlice.ts
+++ src/store/slices/clientSlice.ts
@@ -3,45 +3,48 @@ import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface ClientState {
   clients: TableDetailsTypes[];
-  client: TableDetailsTypes | null;
   status: "idle" | "loading" | "succeeded" | "failed";
   error: unknown;
 }
 
 const initialState: ClientState = {
   clients: [],
-  client: null,
   status: "idle",
   error: null,
 };
 
-const postClientInvite = createAsyncThunk(
-  "clients/post-invites",
-  async (inviteData: TableDetailsTypes, thunkAPI) => {
-    try {
-      const response = localStorage.setItem(
-        "clients",
-        JSON.stringify(inviteData)
-      );
+const postClientInvite = createAsyncThunk<
+  TableDetailsTypes[],
+  TableDetailsTypes
+>("clients/post-invites", async (inviteData: TableDetailsTypes, thunkAPI) => {
+  try {
+    // getting the previous clients stored in json
+    const prevClientsJson = localStorage.getItem("clients");
+    const prevClients: TableDetailsTypes[] = prevClientsJson
+      ? JSON.parse(prevClientsJson)
+      : [];
 
-      console.log("Client Invite:", response);
-    } catch (err) {
-      console.log(err);
-      return thunkAPI.rejectWithValue({
-        message: err,
-        status: err,
-      });
-    }
+    // adding a new client to an array
+    const updatedClients = [...prevClients, inviteData];
+
+    localStorage.setItem("clients", JSON.stringify(updatedClients));
+    return updatedClients;
+  } catch (err) {
+    console.log(err);
+    return thunkAPI.rejectWithValue({
+      message: err,
+      status: err,
+    });
   }
-);
+});
 
-const fetchClientInvite = createAsyncThunk(
+const fetchClientsInvite = createAsyncThunk(
   "clients/fetch-invites",
   async (_, thunkAPI) => {
     try {
       const response = localStorage.getItem("clients");
-      if (!response) return;
-      return JSON.parse(response);
+      if (!response) return [];
+      return JSON.parse(response) as TableDetailsTypes[];
     } catch (err) {
       console.log(err);
       return thunkAPI.rejectWithValue({
@@ -65,39 +68,27 @@ const clientSlice = createSlice({
       })
       .addCase(
         postClientInvite.fulfilled,
-        (
-          state,
-          action: PayloadAction<{
-            clients: TableDetailsTypes[];
-            client: TableDetailsTypes;
-          }>
-        ) => {
+        (state, action: PayloadAction<TableDetailsTypes[]>) => {
           state.status = "succeeded";
-          state.clients = action.payload.clients;
-          state.client = action.payload.client;
+          state.clients = action.payload;
         }
       )
-      .addCase(
-        postClientInvite.rejected,
-        (state, action: PayloadAction<unknown>) => {
-          state.status = "failed";
-          state.error = action.payload;
-        }
-      )
-      .addCase(fetchClientInvite.pending, (state) => {
+      .addCase(postClientInvite.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+      .addCase(fetchClientsInvite.pending, (state) => {
         state.status = "loading";
         state.error = null;
       })
-      .addCase(fetchClientInvite.fulfilled, (state) => {
+      .addCase(fetchClientsInvite.fulfilled, (state, action:PayloadAction<TableDetailsTypes[]>) => {
         state.status = "succeeded";
+        state.clients = action.payload
       })
-      .addCase(
-        fetchClientInvite.rejected,
-        (state, action: PayloadAction<unknown>) => {
-          state.status = "failed";
-          state.error = action.payload;
-        }
-      );
+      .addCase(fetchClientsInvite.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      });
   },
 });
 
